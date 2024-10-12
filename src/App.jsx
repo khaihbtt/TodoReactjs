342aa1dd543a72761e4e5b9a4204408da2259971
khaihbtt <90823991+khaihbtt@users.noreply.github.com>
2024-10-12 14:50:09 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TodoInput from "./components/todoinput"
+import TodoList from "./components/TodoList"
+
+function App() {
+
+  return (
+    <main>
+      <TodoList/>
+      <TodoInput/>
+      
+    </main>
+  )
+}
+
+export default App
